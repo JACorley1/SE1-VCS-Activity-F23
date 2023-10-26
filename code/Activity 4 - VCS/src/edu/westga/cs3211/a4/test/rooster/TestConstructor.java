e326dcf226b6228e36c0337ba5915b7346d161cb
nicklausw <n@nicklausw.net>
2023-10-26 13:33:57 -0400

    Implement Rooster

--- code/Activity 4 - VCS/src/edu/westga/cs3211/a4/test/rooster/TestConstructor.java
+++ code/Activity 4 - VCS/src/edu/westga/cs3211/a4/test/rooster/TestConstructor.java
@@ -4,11 +4,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import edu.westga.cs3211.a4.model.Rooster;
+
 class TestConstructor {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		Rooster r = new Rooster("John", "Silkie");
+		assertEquals(r.getName(), "John", "Name is wrong");
+		assertEquals(r.getBreed(), "Silkie", "Breed is wrong");
 	}
 
 }
