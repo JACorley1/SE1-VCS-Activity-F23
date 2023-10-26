6b9d0871914ae542a62322acba7f53169d5eda32
Trinidena <tdena1@my.westga.edu>
2023-10-26 13:39:29 -0400

    Updated Rooster Class and Test Class

--- code/Activity 4 - VCS/src/edu/westga/cs3211/a4/model/Rooster.java
+++ code/Activity 4 - VCS/src/edu/westga/cs3211/a4/model/Rooster.java
@@ -1,5 +1,18 @@
 package edu.westga.cs3211.a4.model;
 
 public class Rooster {
-
+	private String name;
+	private String breed;
+	
+	public Rooster(String name , String breed) {
+		this.name = name;
+		this.breed = breed;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	public String getBreed() {
+		return this.breed;
+	}
 }
