6b722febb82710d8b526efe9e4da131ce51b8ca0
Jonathan Corley <jcorley@westga.edu>
2023-10-26 09:12:28 -0400

    Adding base project
    Added base project with a variety of simple data classes to be completed in the VCS activity.

---
+++ code/Activity 4 - VCS/src/edu/westga/cs3211/a4/test/monkey/TestCosntructor.java
@@ -0,0 +1,14 @@
+package edu.westga.cs3211.a4.test.monkey;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCosntructor {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ code/Activity 4 - VCS/src/edu/westga/cs3211/a4/test/rat/TestConstructor.java
@@ -0,0 +1,14 @@
+package edu.westga.cs3211.a4.test.rat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestConstructor {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
